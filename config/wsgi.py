b89b0c455452aa7430d975b5692e7f5410319db1
sondh <sondh0127@gmail.com>
2020-02-21 16:57:50 +0700

    fix deploy

--- config/wsgi.py
+++ config/wsgi.py
@@ -17,7 +17,6 @@ import os
 import sys
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 # This allows easy placement of apps within the interior
 # doclabel directory.
@@ -35,4 +34,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
